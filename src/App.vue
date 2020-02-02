ec6c03dcdb9acedc639112fc42b11dea729c8bfc
Seyyed Mehdi Hassanpour <m.hassanpour@mahsan.co>
2020-02-02 19:04:58 +0330

    add icon-eye to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <span class="icon-eye"></span>
+    
   </div>
 </template>
 
